e59beffc5734c6963fdaef0cbb2c61286d07e40e
msesmart <msesmart@gmail.com>
2015-12-04 10:45:12 -0500

    use TreeSet to maintain a sorted set and .floor, .ceiling to find the closest nums.

---
+++ containsDuplicateIII.java
@@ -0,0 +1,13 @@
+public class containsDuplicateIII {
+	public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+		if(k < 1 || t < 0) return false;
+		TreeSet<Long> set = new TreeSet<Long>();
+		for(int i = 0; i < nums.length(); i++) {
+			long temp = (long)nums[i];
+			if((set.floor(temp) != null && temp - set.floor(temp) <= (long)t) || (set.ceiling(c) != null && temp - set.ceiling(c) <= (long)t)) return true;
+			set.add(c);
+			if(i >= k) set.remove((long)nums[i - k]);
+		}
+		return false;
+	}
+}
