0c6b09c5262cd33d46a3103d4c0c45533875b917
msesmart <msesmart@gmail.com>
2015-12-04 10:45:33 -0500

    fix a small bug.

--- containsDuplicateIII.java
+++ containsDuplicateIII.java
@@ -4,7 +4,7 @@ public class containsDuplicateIII {
 		TreeSet<Long> set = new TreeSet<Long>();
 		for(int i = 0; i < nums.length(); i++) {
 			long temp = (long)nums[i];
-			if((set.floor(temp) != null && temp - set.floor(temp) <= (long)t) || (set.ceiling(c) != null && temp - set.ceiling(c) <= (long)t)) return true;
+			if((set.floor(temp) != null && temp - set.floor(temp) <= (long)t) || (set.ceiling(c) != null && set.ceiling(c) - temp <= (long)t)) return true;
 			set.add(c);
 			if(i >= k) set.remove((long)nums[i - k]);
 		}
